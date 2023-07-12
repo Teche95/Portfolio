76ef8b6e9eee5cb7fadfdc77de084a76b9f1d73a
Teche95 <silencertec@gmail.com>
2023-07-12 19:24:37 -0300

    iconos agregados

--- components/Contacto.jsx
+++ components/Contacto.jsx
@@ -2,16 +2,18 @@
 import styles from '@/styles/contacto.module.css'
 import style from "../styles/proyectos.module.css"
 import { Linkedin, Github, Mail } from 'lucide-react';
+
 const Contacto = () => {
 
   return (
     <div >
       <section id="contacto" className={styles.contacto}>
         <h3 className={style.proyectosTextdif}>CONTACTO</h3>
-        <p className={styles.textoContacto}>¡Gracias por visitar mi porfolio! Si tienes alguna idea o proyecto en mente, no dudes en contactarte conmigo.</p>
+        <p className={styles.textoContacto}>¡Gracias por visitar mi portfolio! Si tienes alguna idea o proyecto en mente, no dudes en contactarte conmigo.</p>
         {/* <p>¡Gracias por visitar mi porfolio! Si tienes alguna idea o </p> */}
         {/* <p> proyecto en mente, no dudes en contactarte conmigo.</p> */}
         <div className={styles.container}>
+
           <a
             href={"https://www.linkedin.com/in/gustavo-julian-techeira-bb74681b8/"}
             className={styles.iconLink}>
