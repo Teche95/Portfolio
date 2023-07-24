4f3fc94bc41c5a1af567daefa0c1be6ffa3b1de6
Teche95 <silencertec@gmail.com>
2023-07-24 16:49:27 -0300

    tamaño de contacto modificado

--- components/Contacto.jsx
+++ components/Contacto.jsx
@@ -9,9 +9,7 @@ const Contacto = () => {
     <div >
       <section id="contacto" className={styles.contacto}>
         <h3 className={style.proyectosTextdif}>CONTACTO</h3>
-        <p className={styles.textoContacto}>¡Gracias por visitar mi portfolio! Si tienes alguna idea o proyecto en mente, no dudes en contactarte conmigo.</p>
-        {/* <p>¡Gracias por visitar mi porfolio! Si tienes alguna idea o </p> */}
-        {/* <p> proyecto en mente, no dudes en contactarte conmigo.</p> */}
+        <p className={styles.textoContacto}>¡Gracias por visitar mi portfolio! Si tienes alguna idea o proyecto en mente, no dudes en contactarte conmigo :).</p>
         <div className={styles.container}>
 
           <a
